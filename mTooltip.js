4d4cf235d4c3eeabe3048eaee96f27e429f15fb7
ultimate-tester <jordimueters@hotmail.com>
2015-06-23 10:24:49 +0200

    Added IntelliJ Automation
    Added gitignore, auto formatted source files, configured project to
    automatically create .min.js files.

--- mTooltip.js
+++ mTooltip.js
@@ -1,24 +1,24 @@
-(function($) {
-	$.fn.mTooltip = function(options) {
-		this.each(function(i, e) {
-			var tooltipContent = $(e).attr('data-mTooltipText');
-			var tooltipBox = $();
-			var tempOffset = $(e).offset();
+(function ($) {
+    $.fn.mTooltip = function (options) {
+        this.each(function (i, e) {
+            var tooltipContent = $(e).attr('data-mTooltipText');
+            var tooltipBox = $();
+            var tempOffset = $(e).offset();
 
-			$(e).hover(function() {
-				tooltipBox = $('<div class="mTooltip">'+tooltipContent+'</div>').appendTo('body');
-				tooltipBox.hide();
-				
-				var tooltipLeft = (tempOffset.left + $(e).outerWidth() / 2) - (tooltipBox.outerWidth() / 2);
-				var tooltipTop = tempOffset.top + $(e).outerHeight() / 2 + 20;
-				
-				tooltipBox.css({ position: 'absolute', left: tooltipLeft, top: tooltipTop });
-				tooltipBox.fadeIn();
-			}, function() {
-				tooltipBox.finish().fadeOut('fast', function() {
-					this.remove();
-				});
-			});
-		});
-	};
-} (jQuery));
+            $(e).hover(function () {
+                tooltipBox = $('<div class="mTooltip">' + tooltipContent + '</div>').appendTo('body');
+                tooltipBox.hide();
+
+                var tooltipLeft = (tempOffset.left + $(e).outerWidth() / 2) - (tooltipBox.outerWidth() / 2);
+                var tooltipTop = tempOffset.top + $(e).outerHeight() / 2 + 20;
+
+                tooltipBox.css({position: 'absolute', left: tooltipLeft, top: tooltipTop});
+                tooltipBox.fadeIn();
+            }, function () {
+                tooltipBox.finish().fadeOut('fast', function () {
+                    this.remove();
+                });
+            });
+        });
+    };
+}(jQuery));
